b203d828f26e7c799f4758a60e32216aae6f5dc5
adilsonLopesDev <pdkdidy2011@gmail.com>
2021-07-06 23:07:30 +0100

    feat: create vacancies and details

--- src/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const api = axios.create({
-  baseURL: 'https://api.github.com/',
-  headers: {
-    Authorization: `token ${process.env.NEXT_PUBLIC_PERSONAL_ACCESS_TOKEN}`,
-  }
-})
-
-export default api
